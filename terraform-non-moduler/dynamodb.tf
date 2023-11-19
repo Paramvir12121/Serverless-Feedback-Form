b0988e6e8d3928fd8c28a2fb3cc0e72fa021a6dc
Paramvir Singh <paramvir12121@gmail.com>
2023-11-19 23:51:38 +0000

    dynamodb table added

---
+++ terraform-non-moduler/dynamodb.tf
@@ -0,0 +1,9 @@
+resource "aws_dynamodb_table" "feedback" {
+  name           = var.table_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "FeedbackID"
+  attribute {
+    name = "FeedbackID"
+    type = "S"
+  }
+}
